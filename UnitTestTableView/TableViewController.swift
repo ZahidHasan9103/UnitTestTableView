b2d24fccd4aaba0043e727013a8db9e9d67f74bf
Zahid Hasan <megamind.d.evilll@gmail.com>
2024-07-03 20:40:18 +0600

    Configured TableView

---
+++ UnitTestTableView/TableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TableViewController.swift
+//  UnitTestTableView
+//
+//  Created by ZEUS on 3/7/24.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    private let model = [
+    "One",
+    "Two",
+    "Three"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(model[indexPath.row])
+    }
+}
